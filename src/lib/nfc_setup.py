a021e9fe44bd88a4d677ea77036a39d36463ad52
Grant Birkinbine <grantbirki@github.com>
2022-05-11 22:48:58 -0600

    cleanup

---
+++ src/lib/nfc_setup.py
@@ -0,0 +1,24 @@
+import sys
+
+import board
+import busio
+from adafruit_pn532.i2c import PN532_I2C
+from digitalio import DigitalInOut
+
+
+def nfc_setup():
+    # I2C connection:
+    i2c = busio.I2C(board.SCL, board.SDA)
+
+    reset_pin = DigitalInOut(board.D6)
+    req_pin = DigitalInOut(board.D12)
+    pn532 = PN532_I2C(i2c, debug=False, reset=reset_pin, req=req_pin)
+
+    ic, ver, rev, support = pn532.firmware_version
+    print("[i] Found PN532 with firmware version: {0}.{1}".format(ver, rev))
+    sys.stdout.flush()
+
+    # Configure PN532 to communicate with MiFare cards
+    pn532.SAM_configuration()
+
+    return pn532
--- src/main.py
+++ src/main.py
@@ -1,50 +1,38 @@
-import time
 import sys
+import time
 
-import board
-import busio
-from adafruit_pn532.i2c import PN532_I2C
-from digitalio import DigitalInOut
+from lib import nfc_setup
 
+PN532 = nfc_setup()
 UNLOCK_UIDS = [12345]
 
-# I2C connection:
-i2c = busio.I2C(board.SCL, board.SDA)
-
-reset_pin = DigitalInOut(board.D6)
-req_pin = DigitalInOut(board.D12)
-pn532 = PN532_I2C(i2c, debug=False, reset=reset_pin, req=req_pin)
-
-ic, ver, rev, support = pn532.firmware_version
-print("[i] Found PN532 with firmware version: {0}.{1}".format(ver, rev))
-sys.stdout.flush()
-
-# Configure PN532 to communicate with MiFare cards
-pn532.SAM_configuration()
-
-print("[i] Waiting for RFID/NFC card...\n")
-sys.stdout.flush()
-while True:
-    # Check if a card is available to read
-    uid = pn532.read_passive_target(timeout=0.5)
-
-    # Try again if no card is available.
-    if uid is None:
-        continue
-
-    card_id = ""
-    for id in uid:
-        card_id += str(id)
-    card_id = int(card_id)
-
-    print("[i] Found card with UID:", card_id)
+def main():
+    print("[i] Waiting for RFID/NFC card...\n")
     sys.stdout.flush()
+    while True:
+        # Check if a card is available to read
+        uid = PN532.read_passive_target(timeout=0.5)
 
-    if card_id in UNLOCK_UIDS:
-        print("  [+] Successfully unlocked door!")
-        sys.stdout.flush()
-    else:
-        print("  [-] Invalid access card!")
+        # Try again if no card is available.
+        if uid is None:
+            continue
+
+        card_id = ""
+        for id in uid:
+            card_id += str(id)
+        card_id = int(card_id)
+
+        print("[i] Found card with UID:", card_id)
         sys.stdout.flush()
 
-    time.sleep(3)
+        if card_id in UNLOCK_UIDS:
+            print("  [+] Successfully unlocked door!")
+            sys.stdout.flush()
+        else:
+            print("  [-] Invalid access card!")
+            sys.stdout.flush()
+
+        time.sleep(3)
+
+if __name__ == "__main__":
+    main()
