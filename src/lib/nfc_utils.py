998b7f72bb84823bc66a81c7a3c0837464040a0a
Grant Birkinbine <grantbirki@github.com>
2022-05-11 23:10:17 -0600

    use logger and scan function

--- src/lib/nfc_utils.py
+++ src/lib/nfc_utils.py
@@ -1,5 +1,3 @@
-import sys
-
 import board
 import busio
 from adafruit_pn532.i2c import PN532_I2C
@@ -7,6 +5,25 @@ from digitalio import DigitalInOut
 
 
 class NFCUtils:
+
+    def __init__(self, log):
+        self.pn532 = self.start_i2c()
+        self.log = log
+
+    def scan(self):
+        uid = self.pn532.read_passive_target(timeout=0.5)
+
+        if uid is None:
+            return None
+
+        card_id = ""
+        for id in uid:
+            card_id += str(id)
+        card_id = int(card_id)
+
+        self.log.info(f"Found card with UID: {card_id}")
+
+        return card_id
 
     def start_i2c(self):
         # I2C connection:
@@ -17,8 +34,7 @@ class NFCUtils:
         pn532 = PN532_I2C(i2c, debug=False, reset=reset_pin, req=req_pin)
 
         ic, ver, rev, support = pn532.firmware_version
-        print("[i] Found PN532 with firmware version: {0}.{1}".format(ver, rev))
-        sys.stdout.flush()
+        self.log.info("Found PN532 I2C with firmware version: {0}.{1}".format(ver, rev))
 
         # Configure PN532 to communicate with MiFare cards
         pn532.SAM_configuration()
--- src/main.py
+++ src/main.py
@@ -1,38 +1,25 @@
+import logging
 import sys
-import time
 
 from lib.nfc_utils import NFCUtils
 
-PN532 = NFCUtils().start_i2c()
-UNLOCK_UIDS = [12345]
+LOG = logging.getLogger('logger')
+LOG.basicConfig(format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p', level=logging.DEBUG)
+NFC = NFCUtils(LOG)
 
 def main():
     print("[i] Waiting for RFID/NFC card...\n")
     sys.stdout.flush()
     while True:
         # Check if a card is available to read
-        uid = PN532.read_passive_target(timeout=0.5)
+        uid = NFC.scan()
 
         # Try again if no card is available.
         if uid is None:
             continue
 
-        card_id = ""
-        for id in uid:
-            card_id += str(id)
-        card_id = int(card_id)
-
-        print("[i] Found card with UID:", card_id)
-        sys.stdout.flush()
-
-        if card_id in UNLOCK_UIDS:
-            print("  [+] Successfully unlocked door!")
-            sys.stdout.flush()
-        else:
-            print("  [-] Invalid access card!")
-            sys.stdout.flush()
-
-        time.sleep(3)
+        # Do whatever you want with the card's UID here!
+        # Unlock doors, activate lights, etc
 
 if __name__ == "__main__":
     main()
